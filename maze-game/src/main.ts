a4176f170d7d1a17d5096bad7f74c7e62f95fcc0
Jax Steele <jacquie.steele@evoke-creative.com>
2019-06-19 13:54:12 +0100

    Updated routing in main.ts

--- maze-game/src/main.ts
+++ maze-game/src/main.ts
@@ -1,13 +1,41 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import store from "./store/store";
+// import "./registerServiceWorker";
+
+import Game from "./views/Game.vue";
+import NotFound from "./views/NotFound.vue";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const routes: any = [
+    {
+        path: "/",
+        redirect: "game"
+    },
+    {
+        path: "/game",
+        name: "Game",
+        component: Game
+    },
+    {
+        path: "/not_found",
+        name: "not_found",
+        component: NotFound
+    }
+];
+
+const router: VueRouter = new VueRouter({
+    routes
+});
+
+// tslint:disable-next-line:no-unused-expression
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    el: "#app",
+    router,
+    store,
+    render: h => h(App)
+});
