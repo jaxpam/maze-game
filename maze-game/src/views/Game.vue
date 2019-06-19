b749845abb9ea61504a6b92d6edda102fb9bd36c
Jax Steele <jacquie.steele@evoke-creative.com>
2019-06-19 10:53:06 +0100

    Removed unecessary views and updated main view

--- maze-game/src/views/Game.vue
+++ maze-game/src/views/Game.vue
@@ -7,12 +7,8 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+@Component({})
+
+export default class Game extends Vue {}
 </script>
